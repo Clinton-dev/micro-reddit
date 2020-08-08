c687203126c856541cf4a9db122c9849b42d4c98
clinton wambugu <clintonwambugu@gmail.com>
2020-08-08 21:08:37 +0300

    Validate user

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,5 @@ class Post < ApplicationRecord
   validates :title, presence: true, uniqueness: true, length: { minimum: 2 }
   validates :user_id, presence: true 
   belongs_to :user
+  has_many :comments
 end
